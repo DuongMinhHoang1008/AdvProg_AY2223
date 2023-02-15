117d83bfa7f4eb294b07868a846a5c4cf8a79035
DuongMinhHoang1008 <125247674+DuongMinhHoang1008@users.noreply.github.com>
2023-02-15 14:23:17 +0700

    please correct

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -73,6 +73,10 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer=wordList[index];
+    for(int i=0;i<answer.length();i++)
+    {
+        answer[i]=tolower(answer[i]);
+    }
     return answer;
 }
 
